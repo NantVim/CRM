06229aac78d116bb6ad039951f8ca47ca6032dd0
nantvim <nantvim@gmail.com>
2019-10-06 14:10:29 +0400

    second commit

--- src/main.js
+++ src/main.js
@@ -1,19 +1,46 @@
 import Vue from 'vue';
+import Vuex from 'vuex'
 import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import dateFilter from '@/filters/date.filter';
+import messagePlugin from '@/utils/message.plugin';
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false;
 
+Vue.use(Vuex)
+Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseUrl: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+});
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+});
+
+
